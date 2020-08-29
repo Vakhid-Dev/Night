2aa2a5c18b86aec95dbce53b5bdec90cf9d9099e
Guliev Vakhid <01.unbroken@gmail.com>
2020-08-29 18:41:46 +0300

    Added Data.cs , Type.cs

---
+++ Sample/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample
+{
+    namespace LiteDb
+    {
+        
+    }
+
+    namespace DataTable
+    {
+        
+    }
+   
+}
